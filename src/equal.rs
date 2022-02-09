b43ae7e550f2ac16d929ba3b37b92179c3163524
Federico Barcelona <fede_rico_94@hotmail.com>
2022-02-09 22:12:53 +0100

    feat: Add result matcher

--- src/equal.rs
+++ src/equal.rs
@@ -1,6 +1,7 @@
 use crate::Matcher;
 
 use std::fmt::Debug;
+use std::marker::PhantomData;
 
 /// The EqualMatcher provides equality matching that can be accessed with functions
 /// like [equal], [be_true] or [be_false].
@@ -75,4 +76,94 @@ pub fn be_true() -> EqualMatcher<bool> {
 /// ```
 pub fn be_false() -> EqualMatcher<bool> {
     EqualMatcher { expected: false }
+}
+
+/// Provides equality matching that can be accessed with the function [be_ok].
+pub struct OkResultMatcher<M, T> {
+    inner_matcher: M,
+    phantom: PhantomData<T>,
+}
+
+impl<M, T, E> Matcher<Result<T, E>> for OkResultMatcher<M, T>
+where
+    M: Matcher<T>,
+{
+    fn matches(&self, actual_value: Result<T, E>) {
+        match actual_value {
+            Ok(inner) => {
+                self.inner_matcher.matches(inner);
+            }
+            Err(_) => {
+                debug_assert!(false, "");
+            }
+        }
+    }
+}
+
+/// Provides equality matching that can be accessed with the function [be_err].
+pub struct ErrResultMatcher<M, E> {
+    inner_matcher: M,
+    phantom: PhantomData<E>,
+}
+
+impl<M, T, E> Matcher<Result<T, E>> for ErrResultMatcher<M, E>
+where
+    M: Matcher<E>,
+{
+    fn matches(&self, actual_value: Result<T, E>) {
+        match actual_value {
+            Ok(_) => {
+                debug_assert!(false, "");
+            }
+            Err(inner) => {
+                self.inner_matcher.matches(inner);
+            }
+        }
+    }
+}
+
+/// Expects the provided value to be a result of variant Ok that contains something that matches an inner matcher, for example:
+/// ```
+/// # use expects::equal::{be_ok, be_true};
+/// # use expects::{Matcher, Subject};
+/// Result::<bool,()>::Ok(true).should(be_ok(be_true()))
+/// ```
+/// The following snippet should panic:
+/// ```should_panic
+/// # use expects::equal::{be_ok, be_true};
+/// # use expects::Subject;
+/// Result::<bool,()>::Ok(false).should(be_ok(be_true()));
+/// Result::<bool,()>::Err(()).should(be_ok(be_true()));
+/// ```
+pub fn be_ok<M, T>(expected: M) -> OkResultMatcher<M, T>
+where
+    M: Matcher<T>,
+{
+    OkResultMatcher {
+        inner_matcher: expected,
+        phantom: PhantomData::default(),
+    }
+}
+
+/// Expects the provided value to be a result of variant Err that contains something that matches an inner matcher, for example:
+/// ```
+/// # use expects::equal::{be_err, be_true};
+/// # use expects::{Matcher, Subject};
+/// Result::<(), bool>::Err(true).should(be_err(be_true()))
+/// ```
+/// The following snippet should panic:
+/// ```should_panic
+/// # use expects::equal::{be_err, be_true};
+/// # use expects::Subject;
+/// Result::<(), bool>::Err(false).should(be_err(be_true()));
+/// Result::<(), bool>::Ok(()).should(be_err(be_true()));
+/// ```
+pub fn be_err<M, T>(expected: M) -> ErrResultMatcher<M, T>
+where
+    M: Matcher<T>,
+{
+    ErrResultMatcher {
+        inner_matcher: expected,
+        phantom: PhantomData::default(),
+    }
 }
